1dd6ab5e93c45bd4d68c5867f8668593d648f10f
prasad-bigdp <bigdp585@gmail.com>
2025-06-16 15:22:49 +0530

    update

---
+++ JS/code/first/script.js
@@ -0,0 +1,28 @@
+//This is single line comment
+/* This is 
+multi line 
+comment */
+/*console.log(b)
+var a = 30;
+var a = 50; /* redeclaring */
+/*a = 60;/* reassigning */
+// let b = 20;
+// b = 30;
+// const c = 10;
+// console.log(a)
+// function fun ()
+// {
+//     if (true)
+//     {
+//         var x = 10; let y = 20;
+//     }   
+//     console.log(y)
+// }
+// fun()
+
+
+
+let a = 10, b = 20;
+let c = a + b;
+console.log("the answer is "+c)
+
