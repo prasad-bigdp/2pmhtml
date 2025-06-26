8a62ee5f776ef94328e6d14dc4b0b1efeced5fc0
prasad-bigdp <bigdp585@gmail.com>
2025-06-26 15:26:29 +0530

    update

---
+++ JS/code/arrays/script.js
@@ -0,0 +1,18 @@
+let arr = [5, 4, 2, 6, 7];
+console.log(arr[5]);
+console.log(arr[4]);
+console.log(arr.length);
+arr[100] = 25;
+console.log(arr)
+arr = [10, 20, 30, 40, 50];
+arr.push(60, 70, 80);
+console.log(arr)
+arr.pop();
+console.log(arr)
+arr.unshift(20, 30, 40);
+console.log(arr)
+arr.shift();
+console.log(arr)
+arr = [10, 20, 30, 40, 50];
+arr.splice(2, 1,555,6666,7777)
+console.log(arr)
--- JS/code/functions/script.js
+++ JS/code/functions/script.js
@@ -1,169 +1,206 @@
-// let x = 20;
-// /* WAC to perform different operations as calculator */
-// console.log("choose any one, 1. addition, 2. subtraction, 3. division, 4.multiplication ")
-// let a = 20, b = 10;
-// let ch = 5;
-// switch (ch)
+// // let x = 20;
+// // /* WAC to perform different operations as calculator */
+// // console.log("choose any one, 1. addition, 2. subtraction, 3. division, 4.multiplication ")
+// // let a = 20, b = 10;
+// // let ch = 5;
+// // switch (ch)
+// // {
+// //     case 1: console.log(a + b); break;
+// //     case 2: console.log(a - b); break;
+// //     case 3: console.log(a / b); break;
+// //     case 4: console.log(a + b); break;
+// //     default: console.log("enter valid input")
+// // }
+
+// // /* input: " You are Good", output: "YAG" */
+
+// // let input = "You are Good"
+// // let output = ""
+// // input = input.split(" ")
+// // for (let i of input) {
+// // 	output = output+ i.charAt(0).toUpperCase()
+// // }
+// // console.log(output) //YAG
+
+// // //Input: "PRASAD" ==> "DASARP"
+
+// // input = "PRASAD"
+// // let rev = '';
+// // for (let i = input.length - 1; i >= 0; i--)
+// // {
+// //     rev = rev + input.charAt(i);
+// // }
+
+// // //Input:"This is input" , "sihT si tupni"
+
+
+// // //4 3 2 1
+// // //4 3 2
+// // //4 3
+// // //4
+// // for (let r = 1; r <=4 ; r++)
+// // {
+// //     let str = ''
+// //     for (let j = 4; j >= r; j--)
+// //     {
+// //         str+=j+" "
+// //     }
+// //     console.log(str)
+// // }
+
+// // //       1
+// // //     1 2 1
+// // //    1 3 3 1
+
+
+
+
+
+
+
+// // // 1 2 3 4
+
+
+
+
+// // functions
+// //simple function
+// function display ()
 // {
-//     case 1: console.log(a + b); break;
-//     case 2: console.log(a - b); break;
-//     case 3: console.log(a / b); break;
-//     case 4: console.log(a + b); break;
-//     default: console.log("enter valid input")
+//     console.log("hello world")
 // }
+// display()   // function call
 
-// /* input: " You are Good", output: "YAG" */
-
-// let input = "You are Good"
-// let output = ""
-// input = input.split(" ")
-// for (let i of input) {
-// 	output = output+ i.charAt(0).toUpperCase()
-// }
-// console.log(output) //YAG
-
-// //Input: "PRASAD" ==> "DASARP"
-
-// input = "PRASAD"
-// let rev = '';
-// for (let i = input.length - 1; i >= 0; i--)
+// // function can also accept parameters
+// function sum (a, b)
 // {
-//     rev = rev + input.charAt(i);
+//     console.log(a+b)
 // }
-
-// //Input:"This is input" , "sihT si tupni"
-
-
-// //4 3 2 1
-// //4 3 2
-// //4 3
-// //4
-// for (let r = 1; r <=4 ; r++)
+// console.log(sum);
+// let sum5 = function(a, b)
 // {
-//     let str = ''
-//     for (let j = 4; j >= r; j--)
-//     {
-//         str+=j+" "
-//     }
-//     console.log(str)
+//     console.log(a + b)
+// }
+// sum5(5,3)
+
+
+
+
+
+
+
+
+
+// // function that accept length and breadth and print area of rectangle
+// function area(l, b) {
+// 	console.log(`Area of rectangle is :${l * b}`)
+// }
+// area(10, 20)
+
+// const area2 = function (l, b)
+// {
+//     console.log(`Area of rectangle is :${l * b}`) 
 // }
 
-// //       1
-// //     1 2 1
-// //    1 3 3 1
+// const area3 = (l, b) => console.log(`Area of rectangle is :${l * b}`)
 
-
-
-
-
-
-
-// // 1 2 3 4
-
-
-
-
-// functions
-//simple function
-function display ()
-{
-    console.log("hello world")
-}
-display()   // function call
-
-// function can also accept parameters
-function sum (a, b)
-{
-    console.log(a+b)
-}
-console.log(sum);
-let sum5 = function(a, b)
-{
-    console.log(a + b)
-}
-sum5(5,3)
-
-
-
-
-
-
-
-
-
-// function that accept length and breadth and print area of rectangle
-function area(l, b) {
-	console.log(`Area of rectangle is :${l * b}`)
-}
-area(10, 20)
-
-const area2 = function (l, b)
-{
-    console.log(`Area of rectangle is :${l * b}`) 
-}
-
-const area3 = (l, b) => console.log(`Area of rectangle is :${l * b}`)
-
-// functions can have default value when parameter is not passed
+// // functions can have default value when parameter is not passed
  
-function sum2 (a, b = 6)
-{
-    console.log(a+b)
-}
-sum2(5, 10)
-sum2(5)
+// function sum2 (a, b = 6)
+// {
+//     console.log(a+b)
+// }
+// sum2(5, 10)
+// sum2(5)
 
-// functions can even return the values
+// // functions can even return the values
 
-function sum3(a, b = 6) {
-	return a + b
-}
-let res = sum3(5, 10)
-console.log(res)
+// function sum3(a, b = 6) {
+// 	return a + b
+// }
+// let res = sum3(5, 10)
+// console.log(res)
 
-// WAf to return total characters except spaces in a given string
-//Input: "JS is easy"
-function totalCharWithoutSpace (str)
-{
-    let len = str.length;
-    let spaces = str.split(" ").length - 1
-    return len-spaces
-}
- res= totalCharWithoutSpace("JS is easy")
-console.log(res)
+// // WAf to return total characters except spaces in a given string
+// //Input: "JS is easy"
+// function totalCharWithoutSpace (str)
+// {
+//     let len = str.length;
+//     let spaces = str.split(" ").length - 1
+//     return len-spaces
+// }
+//  res= totalCharWithoutSpace("JS is easy")
+// console.log(res)
     
-// WAF to return the total number of a's and b's
-// Input:"BANANA" , Output: [3 , 1]
-function countAsAndBs (str)
+// // WAF to return the total number of a's and b's
+// // Input:"BANANA" , Output: [3 , 1]
+// function countAsAndBs (str)
+// {
+//     str = str.toLowerCase();
+// 	return [str.split("a").length - 1, str.split("b").length - 1]
+// }
+
+// console.log(countAsAndBs("Banana"))
+
+// // In Js, functions are treated as a variable
+// function display ()
+// {
+//     console.log("simple display")
+// }
+// console.log(display);
+// display()
+// // functions also be written as
+// let display1 = function ()   // function expression
+// {
+//     console.log("simple display")
+// }
+
+
+
+
+
+
+// //WAF to check whether one string is anagram of another string or not
+// // Input: "CAT", "TAC"  , "Prasad", "swtuuu"
+
+
+
+
+
+// function sub (a, b)
+// {
+//     return a - b;
+// }
+// const sub2 = function (a, b) { return a - b; }
+// const sub3 = (a, b) =>  a - b
+
+// res = sub(5, 3);
+// console.log(res);
+
+// callback functions:
+function sum (a,b)
 {
-    str = str.toLowerCase();
-	return [str.split("a").length - 1, str.split("b").length - 1]
+    console.log(a + b);
 }
-
-console.log(countAsAndBs("Banana"))
-
-// In Js, functions are treated as a variable
-function display ()
+console.log(sum) // whole function
+function display (x)
 {
-    console.log("simple display")
+     x(5,3)
 }
-console.log(display);
-display()
-// functions also be written as
-let display1 = function ()   // function expression
+display(sum)
+ 
+
+function outer ()
 {
-    console.log("simple display")
+    let i = 0;
+    function inner ()
+    {
+        i++;
+        console.log(i);
+    }
+    return inner;
 }
-
-
-
-
-
-
-//WAF to check whether one string is anagram of another string or not
-// Input: "CAT", "TAC"  , "Prasad", "swtuuu"
-
-
-
-
-
+res = outer();
+console.log(res)
+res(); //1
+res(); //2
