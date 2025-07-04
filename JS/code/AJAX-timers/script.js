7095294727b8f485d2e790d6f5df90cee6cc04ee
prasad-bigdp <bigdp585@gmail.com>
2025-07-04 15:30:50 +0530

    update

---
+++ JS/code/AJAX-timers/script.js
@@ -0,0 +1,52 @@
+// function display()
+// {
+//     console.log("hi");
+// }
+// display();
+// //setTimeout
+// setTimeout(display,3000)
+
+// setTimeout(function ()
+// {
+//     console.log("hello")
+// }, 0)
+// for (let i = 0; i < 10000; i++)
+// {
+//     console.log("hi")
+// }
+// let c = 0;
+
+// let x = setInterval(function ()
+// {
+//     c++;
+//     document.getElementById('count').textContent = c
+//     if (c == 10000)
+//     {
+//         clearInterval(x);
+//     }
+// },1)
+
+
+// Do a coundown which starts after button click from 10 to 1 for every 1s. After that print "Happy Birthday"
+
+let c = 10;
+// document.getElementById('btn').addEventListener('click', function ()
+// {
+//     let x = setInterval(() =>
+//     {
+//         c--;
+//         document.getElementById('count').textContent = c;
+//         if (c == 0)
+//         {
+//             document.getElementById('count').textContent = "Happy birthday";
+//          clearInterval(x)
+//         }
+//    },1000)
+// })
+
+setInterval(() =>
+{
+    let dt = new Date();
+    console.log(dt.toLocaleTimeString())
+    document.getElementById("count").textContent = dt.toLocaleTimeString()
+},1000)
--- JS/code/events/script.js
+++ JS/code/events/script.js
@@ -1,33 +1,93 @@
-let c=0
-function display ()
+// let c=0
+// function display ()
+// {
+//     c++;
+//     document.getElementById('count').textContent = c;
+// }
+// const btn = document.getElementById("btn");
+// btn.addEventListener('mouseover', display)
+// // btn.addEventListener('click', display2);
+
+// function display2 (e)
+// {
+//     console.log(e.key)
+//     if (e.key == 'a')
+//     {
+//         console.log("hello world")
+//     }
+// }
+// document.addEventListener('keydown', display2)
+
+
+// const inp = document.getElementById('inp');
+// inp.addEventListener('input', function ()
+// {
+//     console.log("hi")
+// })
+
+
+
+
+
+
+const p = document.getElementById('count')
+const button = document.getElementById("btn")
+let c = 0;
+function incr ()
 {
-    c++;
-    document.getElementById('count').textContent = c;
+    // c++;
+    const left = Math.round(Math.random() * 100)
+    const top = Math.round(Math.random() * 100)
+    button.style.position = "relative";
+    button.style.left = `${left}%`;
+    button.style.top = `${top}%`;
+    // p.textContent = c;
+    // button.removeEventListener('click',incr)
 }
-const btn = document.getElementById("btn");
-btn.addEventListener('mouseover', display)
-// btn.addEventListener('click', display2);
+button.addEventListener('mouseover', incr)
 
-function display2 (e)
+
+// document.addEventListener('keydown', function (e)
+// {
+//     console.log(e.key)
+//     if (e.key == 'a')
+//     {
+//         c++
+// 				p.textContent = c
+//     }
+//     else if (e.key == 'm')
+//     {
+//         const left = Math.round(Math.random() * 100)
+// 				const top = Math.round(Math.random() * 100)
+// 				button.style.position = "relative"
+// 				button.style.left = `${left}%`
+// 				button.style.top = `${top}%`
+//     }
+  
+// })
+
+
+document.getElementById('inp').addEventListener('change', function (e)
 {
-    console.log(e.key)
-    if (e.key == 'a')
-    {
-        console.log("hello world")
-    }
-}
-document.addEventListener('keydown', display2)
-
-
-const inp = document.getElementById('inp');
-inp.addEventListener('input', function ()
-{
-    console.log("hi")
+    console.log(e.target.value);
+    document.getElementById('typeText').textContent=e.target.value
 })
 
+// document.addEventListener('', function (e)
+// {
+//     console.log(e)
+// })
 
-
-
-
-
-
+document.getElementById('gp').addEventListener('click', function ()
+{
+    console.log("gp is called");
+},true)
+document.getElementById("p").addEventListener("click", function () {
+	console.log("p is called")
+},true)
+document.getElementById("c").addEventListener("click", function () {
+	console.log("c is called")
+},true)
+document.getElementById("b").addEventListener("click", function () {
+	console.log("button called")
+},true)
