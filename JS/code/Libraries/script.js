dd8a5b53ca5bda163642c800066183486b9ac3c6
prasad-bigdp <bigdp585@gmail.com>
2025-07-09 15:25:43 +0530

    prasad added

---
+++ JS/code/Libraries/script.js
@@ -0,0 +1,21 @@
+// // var typed = new Typed("#name", {
+// // 	strings: ["Developer.", "PRASAD."],
+// //     typeSpeed: 50,
+// //     backSpeed: 50,
+// //     loop:true
+// // })
+// $('#btn').click(function ()
+// {
+//     $('#name').text("Raj")
+// })
+// // gsap.registerPlugin(SplitText);
+
+// // gsap.from('#name', { translateX: '-100vw', duration: 2 })
+// // let split = SplitText.create("#name", {
+// //     type:"chars"
+// // });
+// // split.chars()
+// console.log(window)
+// const name= document.getElementById('name')
+// window.navigator.clipboard.readText().then((res) => console.log(res))
+// .catch((err)=>console.log(err))
